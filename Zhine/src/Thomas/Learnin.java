5d4dbadf9b05a0a17c14b0bb7a9a98143bc996e5
Thompie <Thomasvdb@live.nl>
2015-05-10 19:46:19 +0200

    a

--- Zhine/src/Thomas/Learnin.java
+++ Zhine/src/Thomas/Learnin.java
@@ -1,7 +1,11 @@
 package Thomas;
 
+
 public class Learnin {
 	public static void main(String[] args){
+		Time timeObject = new Time();
+		System.out.println(timeObject.toMilitary());
 		
+		timeObject.setTime(13, 27, 6);
 	}
 }
